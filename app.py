7c2916f7a4c106242ce95be0723a5968cfee0e92
Michał <120658970+mszarkie@users.noreply.github.com>
2023-07-01 19:42:42 +0200

    figured out swagger -> api instead of app

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-
 
 api = Api(app)
 
-app.register_blueprint(ItemBlueprint)
-app.register_blueprint(StoreBlueprint)
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
